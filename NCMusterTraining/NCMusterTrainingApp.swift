c89ebe0814e5fea6298aed85f59818f8e728a0e0
Daniel Clauss <danielclauss@gmx.ch>
2025-02-05 16:55:11 +0100

    Initial Commit

---
+++ NCMusterTraining/NCMusterTrainingApp.swift
@@ -0,0 +1,17 @@
+//
+//  NCMusterTrainingApp.swift
+//  NCMusterTraining
+//
+//  Created by Daniel Clauss on 05.02.2025.
+//
+
+import SwiftUI
+
+@main
+struct NCMusterTrainingApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
